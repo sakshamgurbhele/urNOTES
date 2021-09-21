c9857a6d041a6e088ea3a27b40c701c66e1c79a7
SakshamGurbhele <saksham9013gurbhele@gmail.com>
2021-09-21 16:40:07 +0530

    [Room] NotesDao Query Created

--- app/src/main/java/com/example/urnotes/NotesDao.kt
+++ app/src/main/java/com/example/urnotes/NotesDao.kt
@@ -1,4 +1,18 @@
 package com.example.urnotes
 
-class NotesDao {
+import androidx.lifecycle.LiveData
+import androidx.room.*
+
+@Dao
+interface NotesDao {
+
+    @Insert(onConflict = OnConflictStrategy.IGNORE)
+    suspend fun insert(notes: Notes)
+
+    @Delete
+    suspend fun delete(notes: Notes)
+
+    @Query("SELECT * FROM notes_table ORDER BY id ASC")
+    fun getAllData(): LiveData<List<Notes>>
+
 }
