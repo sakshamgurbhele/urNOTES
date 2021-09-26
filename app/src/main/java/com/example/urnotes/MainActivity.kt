cd2e59717021e493cb7525d5e5adb1aafd388dfc
SakshamGurbhele <saksham9013gurbhele@gmail.com>
2021-09-26 23:23:59 +0530

    RecyclerView Implemented

--- app/src/main/java/com/example/urnotes/MainActivity.kt
+++ app/src/main/java/com/example/urnotes/MainActivity.kt
@@ -2,6 +2,7 @@ package com.example.urnotes
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.LinearLayoutManager
@@ -9,17 +10,29 @@ import androidx.recyclerview.widget.RecyclerView
 
 class MainActivity : AppCompatActivity() {
 
+    val recyclerView = findViewById<RecyclerView>(R.id.recyclerView);
+
     lateinit var viewModel: NoteViewModel
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        recyclerView.layoutManager = LinearLayoutManager(this)
+        val adapter = NotesRVAdapter(this,this)
+        recyclerView.adapter = adapter
+
         viewModel = ViewModelProvider(this,
             ViewModelProvider.AndroidViewModelFactory.getInstance(application)).get(NoteViewModel::class.java)
-
-        viewModel.allNotes.observe(this, Observer {
-
+        viewModel.allNotes.observe(this, Observer {list ->
+            list?.let {
+                adapter.updateList(it)
+            }
         })
-
     }
+
+    fun onItemClick(notes: Notes) {
+        TODO("Not yet implemented")
+    }
+
 }
