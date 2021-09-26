e22af7b0062b2f3049ee6fa5899a760bf82287b6
SakshamGurbhele <saksham9013gurbhele@gmail.com>
2021-09-26 23:26:51 +0530

    Gradle dependency and Delete note function

--- app/src/main/java/com/example/urnotes/NoteViewModel.kt
+++ app/src/main/java/com/example/urnotes/NoteViewModel.kt
@@ -3,11 +3,15 @@ package com.example.urnotes
 import android.app.Application
 import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.LiveData
+import androidx.lifecycle.viewModelScope
 import androidx.room.Dao
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 
 class NoteViewModel(application: Application): AndroidViewModel(application) {
 
     val allNotes: LiveData<List<Notes>>
+
 
     init {
         val database = NotesDatabase.getDatabase(application).getnotesDao()
@@ -15,4 +19,7 @@ class NoteViewModel(application: Application): AndroidViewModel(application) {
         allNotes = repository.AllNotes
     }
 
+    fun deleteNote(notes: Notes) = viewModelScope.launch(Dispatchers.IO){
+
+    }
 }
